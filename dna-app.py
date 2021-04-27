42e9fa3294e25109d35a1cfe56cf11aa3266fdb1
patri-carrasco <patri.carrasco@gmail.com>
2021-04-27 16:54:03 +0200

    Finish DNA

--- dna-app.py
+++ dna-app.py
@@ -1,22 +1,19 @@
-# 6 DNA app
-
+######################
 # 1. Importamos librerías
-~~~python
+######################
 import pandas as pd
 import pandas as pd
 import streamlit as st
 import altair as alt
 from PIL import Image
-~~~
 
 
 
-# 2. Título 
-Cargamos la imagen.
-~~~python
+######################
+# Título 
+######################
 image = Image.open("./images/dna-logo.jpg")
-~~~
-~~~python
+
 st.image(image, use_column_width = True)
 
 st.write("""
@@ -25,14 +22,15 @@ st.write("""
 This app counts the nucleotide composition of query DNA!
 ***
 """)
-~~~
 
-# 3.Entrada de texto
-Texto de entrada.
-~~~python
+######################
+# Text Box
+######################
+
 
 st.header('Enter DNA sequence')
 sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
+
 
 sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
@@ -42,20 +40,16 @@ sequence = ''.join(sequence) # Concatenamos las cadenas
 st.write("""
 ***
 """)
-~~~
-# 4. Imprimimos el DNA de entrada
-~~~python
+# Imprimimos el DNA de entrada
 st.header('INPUT (DNA Query)')
 sequence
-~~~
-# 5. Contar los nucleótidos
-~~~python
+
+# Salida de contar los nucleótidos
 st.header('OUTPUT (DNA Nucleotide Count)')
-~~~
 
 
-### 1. Imprimomos en forma de diccionario
-~~~python
+
+### 1. Print dictionary
 st.subheader('1. Print dictionary')
 def DNA_nucleotide_count(seq):
   d = dict([
@@ -69,28 +63,23 @@ def DNA_nucleotide_count(seq):
 X = DNA_nucleotide_count(sequence)
 
 X
-~~~
-
-### 2. Imprimomos texto
-~~~python
+### 2. Print text
 st.subheader('2. Print text')
 st.write('There are  ' + str(X['A']) + ' adenine (A)')
 st.write('There are  ' + str(X['T']) + ' thymine (T)')
 st.write('There are  ' + str(X['G']) + ' guanine (G)')
 st.write('There are  ' + str(X['C']) + ' cytosine (C)')
-~~~
-### 3. Mostramos en forma de DataFrame
-~~~python
+
+### 3. Display DataFrame
 st.subheader('3. Display DataFrame')
 df = pd.DataFrame.from_dict(X, orient='index')
 df = df.rename({0: 'count'}, axis='columns')
 df.reset_index(inplace=True)
 df = df.rename(columns = {'index':'nucleotide'})
 st.write(df)
-~~~
 
-### 4. Mostramos una visualización de barras
-~~~python
+
+### 4. Display Bar Chart using Altair
 st.subheader('4. Display Bar chart')
 p = alt.Chart(df).mark_bar().encode(
     x='nucleotide',
